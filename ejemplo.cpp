d20a25476f9e2a8ef70c2a9aeb37b9e24ba8f717
dvelal <dvelal@unjbg.edu.pe>
2025-06-19 02:21:05 -0500

    Ejemplo: version funcional

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -11,14 +11,19 @@ struct EMP {
 int main () {
     EMP EMPLEADOS[100];
     int n, k;
+    cout<<"Ingrese cantidad de empleados: ";
     cin>>n;
     float mventa = 0;
     for (int i = 0; i < n; i++){
+        cout<<"-------------------------------"<<endl;
+        cout<<"Ingrese numero de empleado: ";
         cin>>EMPLEADOS[i].num;
+        cout<<"Ingrese el nombre del empleado: ";
         cin.ignore();
         getline(cin, EMPLEADOS[i].nom);
         float vtotales = 0;
         for (int j = 0; j < 12; j++) {
+            cout<<"Ventas del mes "<<j+1<<": ";
             cin>>EMPLEADOS[i].ven[j];
             vtotales = vtotales + EMPLEADOS[i].ven[j];
             //Determinar el empleado con mayores ventas
@@ -27,27 +32,28 @@ int main () {
                 k = i;
             }
         }
+        cout<<"Ingrese el salario del empleado: ";
         cin>>EMPLEADOS[i].sal;
         //incrementamos 10% a empleados con ventas mayor a 100u durante el año"
         if (vtotales > 100)
             EMPLEADOS[i].sal = EMPLEADOS[i].sal * 1.10;
     }
 
-    //VERIFICACION
+    cout<<"---Listado de empleados y salario---"<<endl;
     for (int i = 0; i < n; i++) {
-        cout<<EMPLEADOS[i].num<<endl;
-        cout<<EMPLEADOS[i].nom<<endl;
-        for (int j = 0; j < 12; j++)
-            cout<<EMPLEADOS[i].ven[j]<< " ";
-        cout<<endl;
-        cout<<EMPLEADOS[i].sal<<endl;
+        cout<<"Numero: "<<EMPLEADOS[i].num;
+        cout<<" Nombre: "<<EMPLEADOS[i].nom;
+        cout<<"; Salario: "<<EMPLEADOS[i].sal<<endl;
     }
-    cout<<EMPLEADOS[k].num<<endl;
-    cout<<EMPLEADOS[k].nom<<endl;
+    cout<<"--Empleado con mayores ventas durante el anio--"<<endl;
+    cout<<"Numero: "<<EMPLEADOS[k].num;
+    cout<<" Nombre: "<<EMPLEADOS[k].nom<<endl;
+    
+    cout<<"--Empleados con ventas bajas en diciembre--"<<endl;
     for (int i = 0; i < n; i++) {
         if (EMPLEADOS[i].ven[11] < 30) {
-            cout<<EMPLEADOS[i].num<<endl;
-            cout<<EMPLEADOS[i].nom<<endl;
+            cout<<"Numero: "<<EMPLEADOS[i].num;
+            cout<<" Nombre: "<<EMPLEADOS[i].nom<<endl;
         }
     }
     return 0;
