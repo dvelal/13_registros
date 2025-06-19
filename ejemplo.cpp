819d88754d59552238a4869f638c02edf2e61234
dvelal <dvelal@unjbg.edu.pe>
2025-06-19 00:56:27 -0500

    Ejemplo: listar el numero y nombre del empleado con mayores ventas durante el año

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -10,24 +10,35 @@ struct EMP {
 
 int main () {
     EMP EMPLEADOS[100];
-    int n;
+    int n, k;
     cin>>n;
+    float mventa = 0;
     for (int i = 0; i < n; i++){
         cin>>EMPLEADOS[i].num;
         cin.ignore();
         getline(cin, EMPLEADOS[i].nom);
-        for (int j = 0; j < 12; j++)
+        float vtotales = 0;
+        for (int j = 0; j < 12; j++) {
             cin>>EMPLEADOS[i].ven[j];
+            vtotales = vtotales + EMPLEADOS[i].ven[j];
+            if (vtotales > mventa){
+                mventa = vtotales;
+                k = i;
+            }
+        }
         cin>>EMPLEADOS[i].sal;
     }
+
     //VERIFICACION
     for (int i = 0; i < n; i++) {
-        cout << EMPLEADOS[i].num << endl;
-        cout << EMPLEADOS[i].nom << endl;
+        cout<<EMPLEADOS[i].num<<endl;
+        cout<<EMPLEADOS[i].nom<<endl;
         for (int j = 0; j < 12; j++)
-            cout << EMPLEADOS[i].ven[j] << " ";
-        cout << endl;
-        cout << EMPLEADOS[i].sal << endl;
-}
+            cout<<EMPLEADOS[i].ven[j]<< " ";
+        cout<<endl;
+        cout<<EMPLEADOS[i].sal<<endl;
+    }
+    cout<<EMPLEADOS[k].num<<endl;
+    cout<<EMPLEADOS[k].nom<<endl;
     return 0;
 }
