0a0ca2253defb05364c49d4eaedcbab5ddf5635e
dvelal <dvelal@unjbg.edu.pe>
2025-06-19 01:12:27 -0500

    Ejemplo: Incrementar 10% el salario de los empleados con ventas mayor a 100 unidades durante el año

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -21,12 +21,15 @@ int main () {
         for (int j = 0; j < 12; j++) {
             cin>>EMPLEADOS[i].ven[j];
             vtotales = vtotales + EMPLEADOS[i].ven[j];
+            //Determinar el empleado con mayores ventas
             if (vtotales > mventa){
                 mventa = vtotales;
                 k = i;
             }
         }
         cin>>EMPLEADOS[i].sal;
+        if (vtotales > 100)
+            EMPLEADOS[i].sal = EMPLEADOS[i].sal * 1.10;
     }
 
     //VERIFICACION
@@ -40,5 +43,6 @@ int main () {
     }
     cout<<EMPLEADOS[k].num<<endl;
     cout<<EMPLEADOS[k].nom<<endl;
+
     return 0;
 }
