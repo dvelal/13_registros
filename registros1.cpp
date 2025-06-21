820097e53c2085c329ce7e94efd51b7884166d24
dvelal <dvelal@unjbg.edu.pe>
2025-06-21 12:28:23 -0500

    Ejercicio 1: Version mas funcional

--- registros1.cpp
+++ registros1.cpp
@@ -10,13 +10,16 @@ struct empleado{
 
 int main() {
     int n;
+    cout<<"    ----  EMPLEADOS DE DATATECH  ----"<<endl;
     cout<<"Cantidad de empleados: "; cin>>n;
     empleado E[n];
     for (int i = 0; i < n; i++) {
+        cout<<"---------------------------------"<<endl;
+        cout<<"Nombre: ";
         cin.ignore();
         getline(cin, E[i].nombre);
-        cin>>E[i].sexo;
-        cin>>E[i].sueldo;
+        cout<<"Sexo: "; cin>>E[i].sexo;
+        cout<<"Sueldo: "; cin>>E[i].sueldo;
     }
     cout<<"----Empleado(s) con menor salario----"<<endl;
     double min = E[0].sueldo;
@@ -25,8 +28,11 @@ int main() {
             min = E[i].sueldo;
     }
     for (int i = 0; i < n; i++) {
-        if (E[i].sueldo == min)
-            cout<<"- "<<E[i].nombre<<endl;
+        if (E[i].sueldo == min) {
+            cout<<"- "<<E[i].nombre;
+            cout<<"; sexo: "<<E[i].sexo;
+            cout<<"; salario: "<<E[i].sueldo<<endl;
+        }
     }
     cout<<"----Empleado(s) con mayor salario----"<<endl;
     double max = E[0].sueldo;
@@ -35,8 +41,11 @@ int main() {
             max = E[i].sueldo;
     }
     for (int i = 0; i < n; i++) {
-        if (E[i].sueldo == max)
-            cout<<"- "<<E[i].nombre<<endl;
+        if (E[i].sueldo == max) {
+            cout<<"- "<<E[i].nombre;
+            cout<<"; sexo: "<<E[i].sexo;
+            cout<<"; salario: "<<E[i].sueldo;
+        }
     }
     return 0;
 } 
