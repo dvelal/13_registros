1f7fd92c7eee7b5324a9f060954f733abc5653f8
dvelal <dvelal@unjbg.edu.pe>
2025-06-25 22:03:37 -0500

    Añadimos modificar contactos en contactos.cpp, primer version funcional de la opcion

--- contactos.cpp
+++ contactos.cpp
@@ -16,9 +16,10 @@ struct contactoEmail{
 void leerCorreo(correo &, string, string);
 void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
+void modificarContacto(contactoEmail &, bool, string, bool, char, bool, int, bool, string, string);
 
 int main(){
-    int n, op;
+    int n, op, num;
     string nom, user, domain;
     char sex;
     int edad;
@@ -35,6 +36,7 @@ int main(){
         cout<<"Elige una opcion: "; cin>>op;
         switch(op){
             case 1:
+                system("cls");
                 cout<<"Ingrese los datos de un usuario: "<<endl;
                 cin.ignore();
                 cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
@@ -52,21 +54,64 @@ int main(){
                 system("pause");
                 break;
             case 2:
+                system("cls");
+                cout<<"Ingrese el numero del contacto a modificar: "; cin>>num;
+                if (num > n || num <= 0)
+                    cout<<"Contacto no encontrado."<<endl;
+                else {
+                cout<<"Modificar contacto :::::::::::::::::::::::::::"<<endl;
+                imprimeContacto(lista[num - 1]);
+                char r;
+                bool modNom = false, modSex = false, modEdad = false, modCorreo = false;
+                string nuevoNom, nuevoUser, nuevoDomain;
+                char nuevoSex;
+                int nuevaEdad;
+                cout<<"Modificar nombres? (S/N): "; cin>>r;
+                if (r == 'S' || r == 's') {
+                cin.ignore();
+                cout<<"\tIngrese nombres: ";getline(cin, nuevoNom);
+                modNom = true;
+                }
+                cout<<"Modificar sexo? (S/N): "; cin>>r;
+                if (r == 'S' || r == 's') {
+                cout<<"\tIngrese sexo (M/F): "; cin>>nuevoSex;
+                modSex = true;
+                }
+                cout<<"Modificar edad? (S/N): "; cin>>r;
+                if (r == 'S' || r == 's') {
+                cout<<"\tIngrese edad: "; cin>>nuevaEdad;
+                modEdad = true;
+                }
+                cout<<"Modificar correo electronico? (S/N): "; cin>>r;
+                if (r == 'S' || r == 's') {
+                cout<<"\tIngrese usuario: "; cin>>nuevoUser;
+                cout<<"\tIngrese dominio: "; cin>>nuevoDomain;
+                modCorreo = true;
+                }
 
+                modificarContacto(lista[num - 1], modNom, nuevoNom, modSex, nuevoSex,
+                modEdad, nuevaEdad, modCorreo, nuevoUser, nuevoDomain);
+
+                cout<<"\nContacto modificado correctamente.\n";
+                }
+
+                system("pause");
                 break;
             case 3:
+                system("cls");
                 for(int i = 0; i < n; i++){
-                    cout<<"Contacto #"<<i+1<<endl;
+                    cout<<"Contacto #"<<i+1<<" ------------"<<endl;
                     imprimeContacto(lista[i]);
                     cout<<endl;
                 }
                 system("pause");
                 break;
             case 0:
+                system("cls");
                 char seg, S, N;
                 cout<<"Esta seguro de salir? (S/N): "; cin>>seg;
-                if (seg == N)
-                    op = 1;
+                if (seg == N || seg == n)
+                    op = -1;
                 break;
             default:
                 cout<<"Opcion no valida!"<<endl;
@@ -94,4 +139,15 @@ void imprimeContacto(contactoEmail &c){
     cout<<"Sexo: "<<c.sex<<endl;
     cout<<"Edad: "<<c.edad<<endl;
     cout<<"Email: "<<c.email.user<<"@"<<c.email.domain<<endl;
+}
+
+void modificarContacto(contactoEmail &c, bool modNom, string nuevoNom, bool modSex, char nuevoSex, 
+bool modEdad, int nuevaEdad, bool modCorreo, string nuevoUser, string nuevoDomain) {
+    if (modNom) c.nom = nuevoNom;
+    if (modSex) c.sex = nuevoSex;
+    if (modEdad) c.edad = nuevaEdad;
+    if (modCorreo) {
+        c.email.user = nuevoUser;
+        c.email.domain = nuevoDomain;
+    }
 }
